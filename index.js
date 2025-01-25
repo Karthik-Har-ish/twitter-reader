e46a1e43089ab2387cbd685864329e48430dc2dd
Karthik <karthikh2004@gmail.com>
2025-01-26 04:33:16 +0530

    add boilerplate text and assign the location of a tweet correctly

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const subHeaderEl = document.getElementById("sub-header");
 const logoEl = document.getElementById("logo-el");
 const mainHeaderEl = document.getElementById("main-header");
 let isHeader;
+
 root.onmousedown=(e)=>{
     e.clientY;
     mouseInitPos = e.clientY;
     isHeader = headerEl.dataset.isHeader
-    console.log(isHeader)
     subHeaderEl.animate({
         opacity:"0"
     },{
@@ -25,21 +25,24 @@ root.onmousedown=(e)=>{
 }
 root.onmouseup = (e)=>{
     e.clientY
-    console.log(mouseInitPos>e.clientY)
-    if(mouseInitPos>e.clientY){
-        rectangleEl.animate({
-            opacity:"0"
-        },{
-            duration:300,
-            fill:"forwards"
-        })
+    if(isHeader==="false"){
+        if(mouseInitPos>e.clientY){
+            rectangleEl.animate({
+                opacity:"0"
+            },{
+                duration:300,
+                fill:"forwards"
+            })
 
-        headerEl.style.animation = "reduce-height-of-header 1s"
-        setTimeout(()=>{
-            headerEl.style.height = "110px"
-        },1000)
-        
-        headerEl.dataset.isHeader = "true"
+            headerEl.style.animation = "reduce-height-of-header 1s"
+            setTimeout(()=>{
+                headerEl.style.height = "110px"
+            },1000)
+            
+            headerEl.dataset.isHeader = "true"
+            
+        }
     }
+    
 
 }
---
+++ tweets.js
@@ -0,0 +1,14 @@
+export const array = [
+    {
+        id:1,
+        tweet:"Hello there the angel from my nightmare"
+    },
+    {
+        id:2,
+        tweet:"what is up everybody this is the HasanAbi broadcast coming to you liiiivveee from sunny los angeles california!!"
+    },
+    {
+        id:3,
+        tweet:"vim is trash, vscode better"
+    },
+]
