affb09d374b7065f73868e1c470ea0c37eeb88d6
KarthiK <karthikh2004@gmail.com>
2025-02-07 18:25:39 +0530

    create a second page, have to fix the page animation on reload

--- index.js
+++ index.js
@@ -6,22 +6,28 @@ const subHeaderEl = document.getElementById("sub-header");
 const logoEl = document.getElementById("logo-el");
 const mainHeaderEl = document.getElementById("main-header");
 let isHeader;
+const tweetEl = document.getElementById("tweet")
+const currentTweetEl = document.getElementById("page1")
+const nextTweetEl = document.getElementById("page2")
 
 root.onmousedown=(e)=>{
     e.clientY;
     mouseInitPos = e.clientY;
     isHeader = headerEl.dataset.isHeader
-    subHeaderEl.animate({
+    if(isHeader==="false"){
+        subHeaderEl.animate({
         opacity:"0"
-    },{
+        },{
         duration:300,
         fill:"forwards"
-    })
+        })
+        
     mainHeaderEl.style.animation = "slide-main-header-down 0.3s"
     setTimeout(()=>{
         mainHeaderEl.style.transform = "translateY(20px)"
     })
-    root.style.backgroundColor = "#3f3f3f";
+    root.style.backgroundColor = "#3f3f3f";}
+    
 }
 
 root.onmouseup = (e)=>{
@@ -43,5 +49,12 @@ root.onmouseup = (e)=>{
             headerEl.dataset.isHeader = "true"
             
         }
+        
+    }
+    else{
+        console.log("hello world")
+        currentTweetEl.style.animation = "page-scroll-up 1s"
+        nextTweetEl.style.animation = "page-scroll-up 1s"
+
     }
 }
